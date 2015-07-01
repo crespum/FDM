e8cb8cf9bf57f7368b580f2355bbcf6211a5dcc1
Xabi Crespo <xabicrespog@gmail.com>
2015-07-01 11:51:01 +0200

    Improvements in script

--- utils/event_generator.py
+++ utils/event_generator.py
@@ -1,37 +1,40 @@
 # coding=utf-8
 import time, json
 
-item_type = ('event', 'info', 'ad')
+item_type = ('EVENT', 'INFO', 'AD')
 categories = ('pregon', 'music', 'food', 'sport', 'art', 'fire', 'band')
 places = {
-    'alameda':(41.903501, -8.866704), 
-    'auditorio de san bieito':(41.899915, -8.873203), 
-    'a cruzada':(41.897817, -8.874520), 
-    'as solanas':(41.905435, -8.878922),
-    'rúas da guarda':(-1, -1),
-    'porto':(41.898983, -8.874545),
-    'o fuscalho':(41.902495, -8.879410),
-    'centro cultural':(41.902892, -8.865532),
-    'estadio da sangriña':(41.899626, -8.861348),
-    'montiño':(41.900999, -8.866232),
-    'salcidos':(41.909254, -8.852916)
+    'Alameda':(41.903501, -8.866704), 
+    'Auditorio de San Bieito':(41.899915, -8.873203), 
+    'A Cruzada':(41.897817, -8.874520), 
+    'As Solanas':(41.9038126, -8.8659001),
+    'Rúas de A Guarda':(-1, -1),
+    'Porto':(41.898983, -8.874545),
+    'O Fuscalho':(41.902495, -8.879410),
+    'Centro Cultural':(41.902892, -8.865532),
+    'Estadio A Sangriña':(41.899626, -8.861348),
+    'Montiño':(41.900999, -8.866232),
+    'Salcidos':(41.909254, -8.852916)
     }
+"""
+An event can have the following fields
 event = {
-    'EVENT_NAME':'-1',             # Mandatory
-    'DAY':'-1',                    # Mandatory dd/MM/yyyy
-    'START_TIME':'-1',             # Mandatory hh:mm
-    'END_TIME':'-1',
-    'CATEGORY':'-1',
-    'PLACE':'-1',                  # Mandatory
-    'LATITUDE':'-1',
-    'LONGITUDE':'-1',
-    'DESCRIPTION':'-1',
+    'EVENT_NAME':'',             # Mandatory
+    'DAY':'',                    # Mandatory dd/MM/yyyy
+    'START_TIME':'',             # Mandatory hh:mm
+    'END_TIME':'',
+    'CATEGORY':'',
+    'PLACE':'',                  # Mandatory
+    'LATITUDE':'',
+    'LONGITUDE':'',
+    'DESCRIPTION':'',
     # New fields
-    'PRICE':'-1',
-    'IMG_URL':'-1',
-    'EVENT_URL':'-1',
-    'TYPE':'0'
+    'PRICE':'',
+    'IMG_URL':'',
+    'URL':'',
+    'TYPE':''
 }
+"""
 
 def printDict(d):
     for ind, key in enumerate(d):
@@ -58,18 +61,20 @@ with open("proba.txt", "w") as myfile:
 
 
 while True:
-    new_event = event
+    new_event = {}
 
     print("Tipos de eventos: ")
     printList(item_type)
     new_event['TYPE'] = item_type[int(input("Seleccione un número: "))]
 
-    if new_event['TYPE'] == 'INFO' or new_event['TYPE'] == 'AD':
-        new_event['EVENT_NAME'] = input("Información a mostrar: ")
+    new_event['EVENT_NAME'] = input("Información a mostrar: ")
+    new_event['DAY'] = input("Data dd/MM/yyyy: ")
+    new_event['START_TIME'] = input("Hora de inicio (hh:mm): ")
 
+    if new_event['TYPE'] == 'INFO' or new_event['TYPE'] == 'AD':
         event_url = input("Enlace á información: ")
         if event_url is not '':
-            new_event['EVENT_URL'] = event_url
+            new_event['URL'] = event_url
 
         icon_img_url = input("URL da imaxe do icono: ")
         if icon_img_url is not '':
@@ -77,10 +82,6 @@ while True:
 
 
     if new_event['TYPE'] == 'EVENT':
-        new_event['EVENT_NAME'] = input("Nome do evento: ")
-        new_event['DAY'] = input("Data dd/MM/yyyy: ")
-        new_event['START_TIME'] = input("Hora de inicio (hh:mm): ")
-
         print("Tipos de eventos: ")
         printList(categories)
         new_event['CATEGORY'] = categories[int(input("Seleccionar categoría: "))]
@@ -107,7 +108,7 @@ while True:
 
         event_url = input("URL do evento: ")
         if event_url is not '':
-            new_event['EVENT_URL'] = event_url
+            new_event['URL'] = event_url
 
     print('Engadir o seguinte evento? ')
     print(new_event)
